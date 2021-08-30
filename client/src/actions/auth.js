a54eff2bda5b9ab7069ea3254891939c280080ff
unknown <glodybulambo@gmail.com>
2021-08-30 14:03:33 +0200

    User Login connected to the back end failed

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -3,14 +3,16 @@ import * as api from "../api";
 
 export const signin= (formData,history) => async(dispatch)=>{
 try {
-   //SIGNIN THE USER
+   const {data}= await api.signIn(formData);
+   dispatch({type:AUTH,data});
    history.push('/');
 } catch (error) {console.log(error)}
 }
 
 export const signup= (formData,history) => async(dispatch)=>{
     try {
-       //SIGNUP THE USER
+      const {data}= await api.signUp(formData);
+      dispatch({type:AUTH,data});
        history.push('/');
     } catch (error) {console.log(error)}
     }
