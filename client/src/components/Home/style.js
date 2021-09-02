d8d1b9586039e4bfa494050f07356abe244421a3
unknown <glodybulambo@gmail.com>
2021-09-02 23:21:31 +0200

    chip Input done

--- client/src/components/Home/style.js
+++ client/src/components/Home/style.js
@@ -1,12 +1,21 @@
 import {makeStyles} from "@material-ui/core/styles"
 
 export default makeStyles((theme)=>({
+  appBarSearch: {
+    borderRadius: 4,
+    marginBottom: '1rem',
+    display: 'flex',
+    padding: '16px',
+  },
+  pagination: {
+    borderRadius: 4,
+    marginTop: '1rem',
+    padding: '16px',
+  },
       [theme.breakpoints.down("xs")]:{
-        mainContainer: {
+        gridContainer: {
           flexDirection: 'column-reverse',  
         },
        
-      }
-      
-      
+      },     
 }));
