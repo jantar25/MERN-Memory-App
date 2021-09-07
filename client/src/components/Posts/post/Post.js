aeee21d82437ea390fcb7e341e67914c06f6fee4
unknown <glodybulambo@gmail.com>
2021-09-07 15:10:39 +0200

    Starting post details

--- client/src/components/Posts/post/Post.js
+++ client/src/components/Posts/post/Post.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Card,CardActions,CardContent,CardMedia,Button,Typography } from '@material-ui/core/';
+import { Card,CardActions,CardContent,CardMedia,Button,Typography,ButtonBase} from '@material-ui/core/';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import {useDispatch} from 'react-redux';
+import {useHistory} from 'react-router-dom';
 
 import { deletePost, likePost} from '../../../actions/posts';
 import useStyle from "./style.js";
@@ -12,6 +13,7 @@ import useStyle from "./style.js";
 const Post = ({post,setCurrentId}) => {
     const classes = useStyle();
     const dispatch = useDispatch();
+    const history=useHistory();
     const user= JSON.parse(localStorage.getItem('profile'));
 
 const Likes=()=>{
@@ -22,8 +24,11 @@ const Likes=()=>{
     )}
     return <><ThumbUpAltIcon fontSize='small' />&nbsp;Like</>
 }
+
+const openPost=()=> history.push(`/posts/${post._id}`);
     return (
         <Card className={classes.card} raised elevation={6}>
+            <ButtonBase className={classes.cardAction} onClick={openPost}>
             <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
             <div className={classes.overlay}>
                 <Typography  variant="h5">{post.name}</Typography>
@@ -43,6 +48,7 @@ const Likes=()=>{
             <CardContent>
                 <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
             </CardContent>
+            </ButtonBase>
             <CardActions className={classes.cardActions}>
                 <Button size='small' disabled={!user?.result} onClick={()=>{dispatch(likePost(post._id))}}>
                     <Likes /> 
