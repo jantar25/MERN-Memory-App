a51adc0baba258fe0c8370bf93d672d504fa91cd
unknown <glodybulambo@gmail.com>
2021-09-08 10:13:45 +0200

    Adding styles to  post details

--- client/src/components/PostDetails/PostDetails.jsx
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import {Paper,Typography,CircularProgress,Devider} from '@material-ui/core'
+import {useDispatch,useSelector} from 'react-redux'
+import moment from 'moment'
+import {useParams,useHistory} from 'react-router-dom'
+
+import useStyles from './styles'
 
 const PostDetails = () => {
-    console.log('POSTDETAILS')
+   const {post,posts,isLoading}=useSelector((state)=>state.posts);
     return (
         <div>
             POSTDETAILS
